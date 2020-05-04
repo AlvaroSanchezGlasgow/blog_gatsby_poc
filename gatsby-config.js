81a7390ce4373f6e2564c3d105542dc4ff22c24a
Alvaro Sanchez <alvaro.sanchez.galvan@gmail.com>
2020-05-04 17:52:38 +0100

    new posts and some code analysis

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Tech Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Alvaro Sanchez`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
